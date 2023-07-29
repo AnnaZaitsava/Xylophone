8bfafa8a0eed2d4d733278251bb580743291f2d8
AnnaZaitsava <anya.polden@gmail.com>
2023-07-29 14:19:08 +0300

    Initial Commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Anna Zaitsava on 26.07.23.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        sender.alpha = 0.5
+        
+        playSound(resource: sender.currentTitle!)
+        
+            DispatchQueue.main.asyncAfter(deadline: .now()+0.2) {
+                sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(resource: String) {
+        let url = Bundle.main.url(forResource: resource, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+        
+    
+}
+
+
